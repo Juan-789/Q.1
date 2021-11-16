7d951b01c7cece901a5b3e0f847885602556b7e1
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-11-16 13:29:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 numA = int(input("Give me the first number "))
 numB = int(input("Give me the second number "))
 numC = int(input("Give me the third number "))
+#stores data from user
 print(numA +numB + numC)
